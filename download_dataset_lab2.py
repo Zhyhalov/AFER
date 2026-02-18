d642f7d38d73ba9476ea2714fd5ee93efc8dd81e
Oleksandr Zhyhalov <113834634+AlexandrZhygalov@users.noreply.github.com>
2026-02-18 21:21:48 +0200

    lab_2 task 2 in progress

--- download_dataset_lab2.py
+++ download_dataset_lab2.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("developerghost/water-potability")
+path = kagglehub.dataset_download("rohitgrewal/weather-data")
 
 print("Path to dataset files:", path)
